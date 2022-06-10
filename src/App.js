14b2d7194b406c2188cd841a6ee50f7ee847cbaf
Ehsan ul haq Khawja <uehsan3@icloud.com>
2022-06-10 23:10:24 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,295 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import "./App.css";
+import Refresh from "./refresh.png";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { makeChart, makeWaterData } from "./utils.js";
+import ReactLoading from "react-loading";
+import ReactSelect from "./ReactSelect.js";
+function App() {
+  const url =
+    "https://8h19xk09w6.execute-api.us-west-2.amazonaws.com/default/e_water_quality_multiple_sensors";
+  const [waterData, setWaterData] = useState(null);
+  const [chart1, setChart1] = useState(null);
+  const [chart2, setChart2] = useState(null);
+  const [chart3, setChart3] = useState(null);
+  const [liveData, setLiveData] = useState({});
+  const [refresh, setRefresh] = useState(false);
+  // const [limit, setLimit] = useState(30);
+  const [vw, setVw] = useState();
+  const [index, setIndex] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [period, setPeriod] = useState("last_day");
+  const [sensor, setSensor] = useState("WQ101");
+
+  const destroyCharts = (i) => {
+    if (i === 1) chart1 && chart1.destroy();
+    else if (i === 2) chart2 && chart2.destroy();
+    else if (i === 3) chart3 && chart3.destroy();
+    setVw((0.8 * Math.min(window.innerWidth, window.screen.width)) / 100);
+    setIndex(window.screen.width > 700);
+  };
+  // const handleLimitChange = (e) => {
+  //   setLimit(e.target.value);
+  // };
+
+  useEffect(() => {
+    let x = `${url}?sensor_id=${sensor}&period=${period}`;
+    setLoading(true);
+    axios.get(x).then((response) => {
+      const newData = makeWaterData(response.data, "time_stamp");
+      setWaterData(newData);
+      setLoading(false);
+    });
+  }, [refresh, period, sensor]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setRefresh(!refresh);
+    }, 59 * 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [refresh]);
+
+  useEffect(() => {
+    const last_index = waterData && waterData.dataLabels.length - 1;
+    waterData &&
+      setLiveData({
+        dataLabels: waterData.dataLabels[last_index],
+        tds: waterData.tds[last_index],
+        cod: waterData.cod[last_index],
+        bod: waterData.bod[last_index],
+        ph: waterData.ph[last_index],
+        temp: waterData.temp[last_index],
+        ec: waterData.ec[last_index],
+        dio: waterData.dio[last_index],
+      });
+  }, [waterData]);
+
+  useEffect(() => {
+    destroyCharts(1);
+    let chartType = ["line", "line", "line", "line"];
+    waterData &&
+      setChart1(
+        makeChart(
+          "c1",
+          "DO, COD, BOD and pH",
+          waterData.dataLabels,
+          index
+            ? [
+                "DO mg/L         ",
+                "COD mg/L          ",
+                "BOD mg/L          ",
+                "pH          ",
+              ]
+            : ["DO mg/L", "COD mg/L", "BOD mg/L", "pH"],
+
+          ["", "", "", "1"],
+          [waterData.dio, waterData.cod, waterData.bod, waterData.ph],
+          chartType,
+          ["#ff5e00", "#ff006f", "#0062ff", "#17fc54"],
+          vw,
+          index ? "x" : "y"
+        )
+      );
+  }, [waterData]);
+
+  useEffect(() => {
+    destroyCharts(2);
+    let chartType = ["line", "line"];
+    waterData &&
+      setChart2(
+        makeChart(
+          "c2",
+          "Electrical conductivity and TDS",
+          waterData.dataLabels,
+          index
+            ? ["Electrical conductivity mS/cm          ", "TDS ppm          "]
+            : ["Electrical conductivity mS/cm", "TDS ppm"],
+          ["", "1"],
+          [waterData.ec, waterData.tds],
+          chartType,
+          ["#17fc54", "#595bd9"],
+          vw,
+          index ? "x" : "y"
+        )
+      );
+  }, [waterData]);
+
+  useEffect(() => {
+    destroyCharts(3);
+    let chartType = ["line"];
+    waterData &&
+      setChart3(
+        makeChart(
+          "c3",
+          "Temperature",
+          waterData.dataLabels,
+          index ? ["Temperature ºC          "] : ["Temperature ºC"],
+          [""],
+          [waterData.temp],
+          chartType,
+          ["#ff5e00"],
+          vw,
+          index ? "x" : "y"
+        )
+      );
+  }, [waterData]);
+  const handleSensorChange = (e) => {
+    setSensor(e.value);
+  };
+  return (
+    <div className="App dark">
+      <header className="App-header">
+        WATER &nbsp; QUALITY &nbsp; MONITORING &nbsp; SYSTEM
+      </header>
+      <div id="tabHolder" className="tabHolder">
+        <div id="selectSensor">
+          <span>SENSOR ID</span>
+          <div id="selectSensorInput">
+            <ReactSelect
+              accOpts={["WQ101", "WQ102", "WQ103", "WQ104", "WQ105", "WQ106"]}
+              handleChange={handleSensorChange}
+              them={1}
+              vw={vw}
+            />
+          </div>
+        </div>
+
+        <div id="periodHolder">
+          <div
+            id="period1"
+            className={"tab " + (period === "last_day" ? "selected" : "")}
+            onClick={() => {
+              setPeriod("last_day");
+            }}
+          >
+            DAY
+          </div>
+          <div
+            id="period2"
+            className={"tab " + (period === "last_month" ? "selected" : "")}
+            onClick={() => {
+              setPeriod("last_month");
+            }}
+          >
+            MONTH
+          </div>
+          <div
+            id="period3"
+            className={"tab " + (period === "last_year" ? "selected" : "")}
+            onClick={() => {
+              setPeriod("last_year");
+            }}
+          >
+            YEAR
+          </div>
+        </div>
+      </div>
+      <div className="graphHolder">
+        <div id="g1" className="graph">
+          {loading ? (
+            <div id="loading">
+              <ReactLoading type={"bars"} color={"#2980b9"}></ReactLoading>
+            </div>
+          ) : (
+            <>
+              <div id="liveTitle">
+                <span className="liveTitleText">LIVE DATA</span>
+                <span className="options">
+                  <img
+                    id="refresh"
+                    src={Refresh}
+                    alt="refresh icon"
+                    onClick={() => {
+                      setRefresh(!refresh);
+                    }}
+                  ></img>
+                  {/* <button className="chartTypeButton" onClick={handleChartType}>
+                    {type ? "BAR" : "LINE"}
+                  </button> */}
+                  {/* <span id="limitHolder">
+                    <span>{limit}</span>
+                    <input
+                      type="range"
+                      id="limitRange"
+                      min="10"
+                      max="100"
+                      step="10"
+                      value={limit}
+                      onChange={handleLimitChange}
+                    />
+                  </span> */}
+                </span>
+              </div>
+              <div className="liveDataHolder">
+                <div id="ld0">
+                  <span>Time :</span>{" "}
+                  <span className="liveDataValue">
+                    {liveData && liveData.dataLabels}
+                  </span>
+                </div>
+                <div id="ld1">
+                  <span>Temperature : </span>
+                  <span className="liveDataValue">
+                    {liveData && liveData.temp} ºC
+                  </span>
+                  <span> pH : </span>
+                  <span className="liveDataValue">
+                    {liveData && parseFloat(liveData.ph).toFixed(3)}
+                  </span>
+                </div>
+                <div id="ld2">
+                  <span> TDS : </span>
+                  <span className="liveDataValue">
+                    {liveData && parseFloat(liveData.tds).toFixed(2)} ppm
+                  </span>
+                </div>
+                <div id="ld3">
+                  <span> BOD : </span>
+                  <span className="liveDataValue">
+                    {liveData && parseFloat(liveData.bod).toFixed(2)} mg/L
+                  </span>
+                </div>
+                <div id="ld4">
+                  <span> COD : </span>
+                  <span className="liveDataValue">
+                    {liveData && parseFloat(liveData.cod).toFixed(2)} mg/L
+                  </span>
+                </div>
+                <div id="ld5">
+                  <span>DO : </span>
+                  <span className="liveDataValue">
+                    {liveData && parseFloat(liveData.dio).toFixed(2)} mg/L
+                  </span>
+                </div>
+                <div id="ld6">
+                  <span>Electrical Conductivity : </span>
+                  <span className="liveDataValue">
+                    {liveData && parseFloat(liveData.ec).toFixed(3)} mS/cm
+                  </span>
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+        <div id="g2" className="graph">
+          <div className="chart">
+            <canvas id="c1"></canvas>
+          </div>
+        </div>
+        <div id="g3" className="graph">
+          <div className="chart">
+            <canvas id="c2"></canvas>
+          </div>
+        </div>
+        <div id="g4" className="graph">
+          <canvas id="c3"></canvas>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils.js
@@ -0,0 +1,211 @@
+import moment from "moment";
+import Chart from "chart.js/auto";
+
+export const makeWaterData = (arr, key = "Timestamp") => {
+  //no need to sort data, data comes sorted from api
+  // arr.sort((a, b) => timeDateSorter(a, b, key));
+  let [dataLabels, tds, cod, bod, ph, temp, ec, dio] = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+  ];
+  for (let i = 0; i < arr.length; i++) {
+    dataLabels.push(moment(toDate(arr[i][key])).format("lll"));
+    tds.push(Math.abs(arr[i]["TDSmg/l"]) > 5000 ? 0 : arr[i]["TDSmg/l"]);
+    cod.push(Math.abs(arr[i]["CODmg/l"]) > 100 ? 0 : arr[i]["CODmg/l"]);
+    bod.push(Math.abs(arr[i]["BODmg/l"]) > 100 ? 0 : arr[i]["BODmg/l"]);
+    ph.push(Math.abs(arr[i]["ph"]) > 20 ? 0 : arr[i]["ph"]);
+    temp.push(
+      Math.abs(arr[i]["Temperature(Air/Water)"]) > 101
+        ? 0
+        : arr[i]["Temperature(Air/Water)"]
+    );
+    ec.push(Math.abs(arr[i]["CondUs/Cm"]) > 5000 ? 0 : arr[i]["CondUs/Cm"]);
+    dio.push(10.27 - 0.6 * parseFloat(cod[i]) - 0.35 * parseFloat(bod[i]));
+  }
+
+  return { dataLabels, tds, cod, bod, ph, temp, ec, dio };
+};
+
+// export const timeDateSorter = (_a, _b, key) => {
+//   let a = _a[key];
+//   let b = _b[key];
+
+//   let p = toDate(a);
+//   let q = toDate(b);
+//   if (p < q) return -1;
+//   else if (p > q) return 1;
+//   return 0;
+// };
+
+function toDate(a) {
+  a =
+    a.substring(5, 7) +
+    "/" +
+    a.substring(8, 10) +
+    "/" +
+    a.substring(0, 4) +
+    " " +
+    a.substring(11, 19);
+  return new Date(a);
+}
+
+export const makeChart = (
+  canvasId,
+  title,
+  xLabel,
+  yLabelArr,
+  axisIDArr,
+  yDataArr,
+  type,
+  colorArr = [],
+  fontSize = 14,
+  indexAxis = "x"
+) => {
+  if (colorArr === []) {
+    yDataArr.forEach((_, i) => {
+      colorArr[i] = `hsl(
+        ${genRan(0, 360)},
+        100%,100%
+      )`;
+    });
+  }
+  if (fontSize < 8) fontSize = 8;
+  let scales = {
+    y: {
+      position: "left",
+      beginAtZero: true,
+      grid: {
+        display: true,
+        color: "#ffffff11",
+      },
+      ticks: {
+        font: { size: fontSize },
+      },
+    },
+    x: {
+      position: "bottom",
+      beginAtZero: true,
+      ticks: {
+        maxRotation: 0,
+        autoskip: true,
+        autoSkipPadding: 20,
+        font: { size: fontSize },
+      },
+      grid: {
+        display: false,
+      },
+    },
+  };
+
+  const datasets = yDataArr.map((yData, i) => {
+    if (axisIDArr[i] === "1") {
+      if (indexAxis === "x") {
+        scales.y1 = {
+          position: "right",
+          beginAtZero: true,
+          grid: {
+            display: false,
+          },
+          ticks: {
+            color: hexWithAlpha(colorArr[i], "dd"),
+            font: { size: fontSize },
+          },
+        };
+      } else {
+        scales.x1 = {
+          position: "top",
+          barPercentage: 1.0,
+          categoryPercentage: 0.5,
+          beginAtZero: true,
+          grid: {
+            display: false,
+          },
+          ticks: {
+            color: hexWithAlpha(colorArr[i], "dd"),
+            font: { size: fontSize },
+          },
+        };
+      }
+    }
+    let xId;
+    let yId;
+    if (axisIDArr[i] === "1") {
+      if (indexAxis === "x") {
+        yId = "y1";
+      }
+      xId = "x1";
+    }
+    let ret = {
+      type: type[i],
+      label: yLabelArr[i],
+      data: yData,
+      backgroundColor: hexWithAlpha(colorArr[i], "55"),
+      borderColor: colorArr[i],
+      borderWidth: 1,
+
+      tension: 0,
+    };
+    if (indexAxis === "x") {
+      ret.yAxisID = yId;
+    } else ret.xAxisID = xId;
+    return ret;
+  });
+
+  return new Chart(document.getElementById(canvasId).getContext("2d"), {
+    type: "bar",
+    data: {
+      labels: xLabel,
+      datasets: datasets,
+    },
+    options: {
+      indexAxis: indexAxis,
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: scales,
+      plugins: {
+        title: {
+          display: true,
+          text: title,
+          position: "top",
+          align: "start",
+          color: "#00c3ff",
+          font: {
+            size: fontSize * 2,
+            weight: "200",
+          },
+        },
+        legend: {
+          position: "top",
+          align: "end",
+          labels: {
+            color: "#00c3ff",
+            display: true,
+            font: {
+              size: fontSize,
+            },
+          },
+        },
+      },
+      elements: {
+        point: {
+          radius: 1,
+          hoverRadius: 10,
+        },
+      },
+    },
+  });
+};
+export const genRan = (min, max) => {
+  let x = min + Math.floor(Math.random() * (max - min));
+  console.log(x);
+
+  return x;
+};
+
+export const hexWithAlpha = (hex, alpha) => `${hex}${alpha}`;
